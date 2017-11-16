8df51c88d9e755f7b41f513fc88b91102bf45e98
zhouhj <zhouhj5@asiainfo.com>
2017-11-16 18:41:34 +0800

    frame-work

---
+++ train.py
@@ -0,0 +1,18 @@
+'''
+Created on Nov 16, 2017
+
+@author: 
+'''
+def Train(epoch):
+    return 1
+
+
+startEpoch=0
+nEpochs=1
+for epoch in range(startEpoch, nEpochs):
+    learning_rate = 0.001
+    train_loss = Train(epoch)
+    
+    print('training loss for epoch ' + str(epoch)  + ' is : ' + str(train_loss))
+    
+    
